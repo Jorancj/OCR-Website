52b375f33e8679ab2993f4227afc5b653164b8ab
Jorancj <69878121+Jorancj@users.noreply.github.com>
2021-04-13 17:13:07 +0200

    jalla bar

--- static/index.js
+++ static/index.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
+    $("#progress").hide();
 
     $("#uploadForm").submit(function (){
+        $("#progress").show();
        $(this).ajaxSubmit({
            error:function (xhr){
                console.log(xhr);
@@ -8,9 +10,10 @@ $(document).ready(function(){
            success:function (res){
                $("#OCROutput").text(res.message);
                $("#OCROutput").css('color', '#212121');
+               $("#progress").hide();
            }
        });
-       $(this).hide();
+
        return false;
     });
 
